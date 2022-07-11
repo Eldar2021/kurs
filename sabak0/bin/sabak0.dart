ac842766cc1fece0623bf21223e89033145be7a3
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-07-11 21:53:27 +0600

    added sabak0

---
+++ sabak0/bin/sabak0.dart
@@ -0,0 +1,7 @@
+// import 'package:sabak0/sabak0.dart' as sabak0;
+
+void main(List<String> arguments) {
+  // while (false) {
+  //   print('object');
+  // }
+}
---
+++ sabak0/test/sabak0_test.dart
@@ -0,0 +1,8 @@
+import 'package:sabak0/sabak0.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
