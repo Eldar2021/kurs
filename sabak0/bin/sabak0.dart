9b31b37f545b0bad73409c34f5d60e9892e96f5a
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-07-19 16:07:25 +0600

    lesson with Mustafa

--- sabak0/bin/sabak0.dart
+++ sabak0/bin/sabak0.dart
@@ -1,7 +1,34 @@
-// import 'package:sabak0/sabak0.dart' as sabak0;
+void main() {
+  // final y1 = StaticClass._();
+  // final y2 = StaticClass();
 
-void main(List<String> arguments) {
-  // while (false) {
-  //   print('object');
-  // }
+  // print(y1.abs);
+  print(StaticClass.abs);
+  print(StaticClass.bsa);
+
+  print('Singleton');
+
+  print(StaticClass.bsa);
+
+  final x1 = StaticClassEmes();
+  final x2 = StaticClassEmes();
+  // print(x1.abs);
+
+  // print(x2.bsa);
+
+  print('Is Not Singleton');
+
+  print(x1 == x2);
+}
+
+class StaticClass {
+  StaticClass._();
+
+  static const String abs = 'cihuwehq';
+  static const String bsa = 'qbdkqbdw';
+}
+
+class StaticClassEmes {
+  final String abs = 'wkenfoe';
+  final String bsa = 'qbdkqbdw';
 }
