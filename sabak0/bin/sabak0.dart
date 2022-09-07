405a3dbe55c6dd4ddd405f2f09e62ba261cc9273
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-09-07 15:01:30 +0600

    added timeout

--- sabak0/bin/sabak0.dart
+++ sabak0/bin/sabak0.dart
@@ -1,34 +1,57 @@
-void main() {
-  // final y1 = StaticClass._();
-  // final y2 = StaticClass();
+void main() async {
+  var result = await salam().timeout(const Duration(seconds: 3));
+  print(result); // 'completed'
 
-  // print(y1.abs);
-  print(StaticClass.abs);
-  print(StaticClass.bsa);
+//   result = await salam()
+//       .timeout(const Duration(seconds: 1), onTimeout: () => 'timeout');
+//   print(result); // 'timeout'
 
-  print('Singleton');
-
-  print(StaticClass.bsa);
-
-  final x1 = StaticClassEmes();
-  final x2 = StaticClassEmes();
-  // print(x1.abs);
-
-  // print(x2.bsa);
-
-  print('Is Not Singleton');
-
-  print(x1 == x2);
+//   result = await salam()
+//       .timeout(const Duration(seconds: 2)); // Throws.
 }
 
-class StaticClass {
-  StaticClass._();
-
-  static const String abs = 'cihuwehq';
-  static const String bsa = 'qbdkqbdw';
+Future<String> salam() async {
+  print('bastaldy');
+  await Future.delayed(Duration(seconds: 10));
+  print('salam');
+  return 'Eldiyar';
 }
 
-class StaticClassEmes {
-  final String abs = 'wkenfoe';
-  final String bsa = 'qbdkqbdw';
-}
+
+
+
+
+// void main() {
+//   // final y1 = StaticClass._();
+//   // final y2 = StaticClass();
+
+//   // print(y1.abs);
+//   print(StaticClass.abs);
+//   print(StaticClass.bsa);
+
+//   print('Singleton');
+
+//   print(StaticClass.bsa);
+
+//   final x1 = StaticClassEmes();
+//   final x2 = StaticClassEmes();
+//   // print(x1.abs);
+
+//   // print(x2.bsa);
+
+//   print('Is Not Singleton');
+
+//   print(x1 == x2);
+// }
+
+// class StaticClass {
+//   StaticClass._();
+
+//   static const String abs = 'cihuwehq';
+//   static const String bsa = 'qbdkqbdw';
+// }
+
+// class StaticClassEmes {
+//   final String abs = 'wkenfoe';
+//   final String bsa = 'qbdkqbdw';
+// }
