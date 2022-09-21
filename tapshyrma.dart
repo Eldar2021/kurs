3479235cf9d6c1630d2db1e0a835ce6aec69e2fe
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-09-21 23:59:19 +0600

    added home work

---
+++ tapshyrma.dart
@@ -0,0 +1,12 @@
+/*
+https://play.google.com/store/apps/details?id=com.asmolgam.capitals&hl=en_SG&gl=US
+
+Arsen Evropa
+Mairambek Bayke Asia
+Meerim eje Tunduk Amerika
+Aydar Bayke Yushtuk Amerika
+Altynbek Baike Africa
+Bakyt Bayke Avstraliya
+Talant en kichine 15 mamleket
+Adilbek Bayke en chong 15 mamleket
+*/
