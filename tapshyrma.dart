8f87c1dbf162a65d5ef9fa68e303ac46ffc95c2b
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-09-22 00:01:24 +0600

    added link

--- tapshyrma.dart
+++ tapshyrma.dart
@@ -1,6 +1,4 @@
 /*
-https://play.google.com/store/apps/details?id=com.asmolgam.capitals&hl=en_SG&gl=US
-
 Arsen Evropa
 Mairambek Bayke Asia
 Meerim eje Tunduk Amerika
@@ -9,4 +7,8 @@ Altynbek Baike Africa
 Bakyt Bayke Avstraliya
 Talant en kichine 15 mamleket
 Adilbek Bayke en chong 15 mamleket
+
+Koldonuu
+https://play.google.com/store/apps/details?id=com.asmolgam.capitals&hl=en_SG&gl=US
+https://api.flutter.dev/flutter/widgets/GridView-class.html
 */
