7585907ff4cf5ee33212fa339413d7038cdd1abd
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-10-06 00:23:25 +0600

    added weather app 2

---
+++ weather_app2/lib/components/custom_icon_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../constants/app_colors.dart';
+
+class CustomIconButton extends StatelessWidget {
+  const CustomIconButton({
+    Key? key,
+    required this.icon,
+  }) : super(key: key);
+
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {},
+      iconSize: 50,
+      color: AppColors.white,
+      icon: Icon(icon),
+    );
+  }
+}
