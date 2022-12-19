59cd8ed123a6ca00e9d0d009180893f3f3c427ef
Eldar2021 <eldiiaralmazbekov@gmail.com>
2022-12-19 22:48:36 +0600

    added bloc01

---
+++ bloc01/lib/example2/detail_page2.dart
@@ -0,0 +1,24 @@
+import 'package:bloc01/example2/home_page2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class Detail2Page extends StatelessWidget {
+  const Detail2Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: BlocBuilder<CounterCubit, int>(
+          builder: (context, state) {
+            return Text('Detail2Page $state');
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.read<CounterCubit>().decrement(),
+        child: const Icon(Icons.remove),
+      ),
+    );
+  }
+}
